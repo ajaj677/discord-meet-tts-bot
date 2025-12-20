e175aeaa155c73f4873b02048f4507c6625d49df
ajaj677 <ajajm677@gmail.com>
2025-12-20 23:57:52 +0530

    work on functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-heading = document.querySelectorAll("Heading");
+const parentBtn = document.querySelector(".button")
+const selectMeetBtn = document.querySelector(".selectMeet");
+const selectDiscordBtn = document.querySelector(".selectDiscordVC");
 
-i
+const disableBtn = (BtnElement) => {
+    if (BtnElement!=selectDiscordBtn) {
+        BtnElement.disabled = true;
+        BtnElement.style.backgroundColor = "Grey";
+    } 
+
+};
+
+parentBtn.addEventListener("click", () => {
+    disableBtn(selectDiscordBtn);
+});
+
+
