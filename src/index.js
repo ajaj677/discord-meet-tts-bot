f9542548a88100ff62c20f230ae823f9cce62a47
ajaj677 <ajajm677@gmail.com>
2025-12-21 22:55:00 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const parentBtn = document.querySelector(".button")
 const selectMeetBtn = document.querySelector(".selectMeet");
 const selectDiscordBtn = document.querySelector(".selectDiscordVC");
+console.log("Script loaded successfully");
 
 const disableBtn = (BtnElement) => {
     if (BtnElement!=selectDiscordBtn) {
         BtnElement.disabled = true;
         BtnElement.style.backgroundColor = "Grey";
     } 
-
 };
 
 parentBtn.addEventListener("click", () => {
